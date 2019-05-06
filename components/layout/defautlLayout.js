3ecaee20795c349a14a38d9f63d04902a6dfd20c
chkui <chenkui@chkui.com>
2019-05-07 00:29:22 +0800

    adde code

---
+++ components/layout/defautlLayout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DefaultLayout = props => (<div>
+
+</div>);
+
+const Content = props => {
+    const {Component} = props;
+    return (<div><Component/></div>)
+};
